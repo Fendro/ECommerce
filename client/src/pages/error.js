782202417f7f60b7c67d0e606b23f63677f80e80
maximaron <maksym.prokopovych@epitech.eu>
2023-07-26 15:34:18 +0200

    Added error page

---
+++ client/src/pages/error.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Container } from '@mui/material';
+import Header from '../components/header';
+import { styled } from '@mui/material/styles';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: black;
+    color: white;
+  }
+`;
+function ErrorPage() {
+    return (
+        <>
+            <Header />
+            <CenteredContainer>
+            <h1>Page not founnd</h1>
+            </CenteredContainer>
+            <GlobalStyle />
+        </>
+    )
+}
+export default ErrorPage;
+
+const CenteredContainer = styled(Container)({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+});
