7593b2f72bfaaca5dd4bc7a37f6ab94e5824cc0e
Baptiste-R-epi <baptiste.royer@epitech.eu>
2023-08-02 16:19:18 +0200

    change urlFetch to accept array in addition to string

--- client/src/utils/urlFetch.js
+++ client/src/utils/urlFetch.js
@@ -1,9 +1,18 @@
 export function urlFetch(path, args) {
-	let url = "http://localhost:4242/" + path + "?";
-	if (args) {
-		Object.keys(args).forEach(key => {
+	let url = "http://localhost:4242/" + path;
+	if (!args)
+		return url;
+	url += "?";
+
+	Object.keys(args).forEach(key => {
+		if ((typeof args[key]) === "object") {
+			args[key].forEach(value => {
+				url += key + "=" + value + "&"
+			})
+		} else {
 			url += key + "=" + args[key] + "&"
-		});
-	}
+		}
+	});
+
 	return url.slice(0, -1);
 }
