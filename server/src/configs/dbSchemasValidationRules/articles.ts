ec40f3cb868c08baf413eae24f02e3e888241570
Fendro <tristan.schmitt@epitech.eu>
2023-08-03 14:24:11 +0200

    Logger structure defined. Fixed data validation username description and requirements.

--- server/src/configs/dbSchemasValidationRules/articles.ts
+++ server/src/configs/dbSchemasValidationRules/articles.ts
@@ -16,7 +16,7 @@ module.exports = {
     properties: {
       categories: {
         bsonType: "array",
-        description: "must be an array of strings",
+        description: "must be an array of strings.",
         items: {
           bsonType: "string",
         },
@@ -24,11 +24,15 @@ module.exports = {
       },
       description: {
         bsonType: "string",
-        description: "must be a string and is required",
+        description: "must be a string and is required.",
+      },
+      featured: {
+        bsonType: "bool",
+        description: "must be a boolean",
       },
       images: {
         bsonType: "array",
-        description: "must be an array of strings and is required",
+        description: "must be an array of strings and is required.",
         items: {
           bsonType: "string",
         },
@@ -36,23 +40,23 @@ module.exports = {
       },
       name: {
         bsonType: "string",
-        description: "must be a string and is required",
+        description: "must be a string and is required.",
       },
       price: {
         bsonType: ["double", "int", "long"],
-        description: "must be a number and is required",
+        description: "must be a number and is required.",
       },
       quantity: {
         bsonType: ["double", "int", "long"],
-        description: "must be a number and is required",
+        description: "must be a number and is required.",
       },
       searches: {
         bsonType: ["double", "int", "long"],
-        description: "must be a number and is required",
+        description: "must be a number and is required.",
       },
       specs: {
         bsonType: "array",
-        description: "must be an array of strings and is required",
+        description: "must be an array of strings and is required.",
         items: {
           bsonType: "string",
         },
@@ -60,7 +64,7 @@ module.exports = {
       },
       views: {
         bsonType: ["double", "int", "long"],
-        description: "must be a number and is required",
+        description: "must be a number and is required.",
       },
     },
   },
