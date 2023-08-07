c41ca2d1be229b2a346804462f9e7fca761624f6
maximaron <maksym.prokopovych@epitech.eu>
2023-08-07 15:06:27 +0200

    Added random articles to show functionality of the page. Added button login on header for guest

--- client/src/components/form/cartForm.js
+++ client/src/components/form/cartForm.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material';
 import { TopCenterContainer } from '../styling';
@@ -6,61 +6,76 @@ import ReactModal from 'react-modal';
 import { urlFetch } from "../../utils/urlFetch";
 import { CheckContext } from '../../context/CheckContext';
 ReactModal.setAppElement('body');
+
+
+const arts = [
+    { id: 1, name: 'Product 1', price: 10,test: 1 },
+    { id: 2, name: 'Product 2', price: 20,test: 2 },
+    { id: 3, name: 'Product 3', price: 15,test: 3 },
+]
 export default function CartForm() {
     const navigate = useNavigate();
     const [isOpen, setIsOpen] = useState(false);
     const { check, setCheck } = useContext(CheckContext);
-    const checkUser = async () =>{
+
+    useEffect(() => {
+        checkUser();
+    }, []);
+
+    const checkUser = async () => {
         try {
             const res = await fetch(urlFetch("auth/login"), {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    credentials: "include",
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
                 },
-            );
+                credentials: "include",
+            });
             const json = await res.json();
-            if (json.message == "A user is already logged in."){
+            if (json.message === "A user is already logged in.") {
                 setCheck(true);
-            }else{
+            } else {
                 setCheck(false);
             }
         } catch (error) {
+            console.error(error);
         }
-    }
-    checkUser();
-    if (check === true){
-        return (
-            <>
-                <TopCenterContainer>
-                    <h1>##List of articles##</h1>
+    };
+
+
+    return (
+        <>
+            <TopCenterContainer>
+                <h1>List of articles</h1>
+            </TopCenterContainer>
+            {arts.map(product => (
+                <TopCenterContainer key={product.id}>
+                    <h2>{product.name}</h2>
+                    <p>Price: ${product.price}</p>
+                    <p>Quantity:{product.test}</p>
                 </TopCenterContainer>
+            ))}
+            {check ? (
                 <TopCenterContainer>
                     <Button
                         variant="outlined"
                         color="success"
-                        onClick={() => navigate("/delivery")}>
+                        onClick={() => navigate("/delivery")}
+                    >
                         Order
                     </Button>
                 </TopCenterContainer>
-
-            </>
-        )
-    }
-    return (
-        <>
-            <TopCenterContainer>
-                <h1>##List of articles##</h1>
-            </TopCenterContainer>
-            <TopCenterContainer>
-                <Button
-                    variant="outlined"
-                    color="success"
-                    onClick={() => setIsOpen(true)}>
-                    Order
-                </Button>
-            </TopCenterContainer>
+            ) : (
+                <TopCenterContainer>
+                    <Button
+                        variant="outlined"
+                        color="success"
+                        onClick={() => setIsOpen(true)}
+                    >
+                        Order
+                    </Button>
+                </TopCenterContainer>
+            )}
             <ReactModal
                 isOpen={isOpen}
                 contentLabel="Test"
@@ -82,7 +97,7 @@ export default function CartForm() {
                         top: '50%',
                         left: '35%',
                         transform: 'translate(-50%, -50%)',
-                    }
+                    },
                 }}
             >
                 <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
@@ -92,7 +107,8 @@ export default function CartForm() {
                         color="success"
                         onClick={() => {
                             navigate("/delivery");
-                        }}>
+                        }}
+                    >
                         Continuer en tant qu'invité
                     </Button>
                     <Button
@@ -101,11 +117,12 @@ export default function CartForm() {
                         color="success"
                         onClick={() => {
                             navigate("/login");
-                        }}>
+                        }}
+                    >
                         Se connecter
                     </Button>
                 </div>
             </ReactModal>
         </>
-    )
+    );
 }
