18e7e36c694cc3e7b92da7cb3874a6ea26776a61
maximaron <maksym.prokopovych@epitech.eu>
2023-08-09 11:08:48 +0200

    We can add an article to the cart

--- client/src/components/form/ArticleForm.js
+++ client/src/components/form/ArticleForm.js
@@ -1,90 +1,98 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import {
-  AnyDiv,
-  AnyImage,
-  AnyText,
-  ArticleContainer,
-  Linebreak,
-} from "../styling";
-import { serverURL } from "../../utils/serverURL";
+import React, {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import {AnyDiv, AnyImage, AnyText, ArticleContainer, Linebreak} from "../styling";
+import {serverURL} from "../../utils/serverURL";
+import {Button} from "@mui/material";
 
 export default function Product() {
-  const { id } = useParams();
-  const [fetchRes, setFetchRes] = useState("loading");
-  const [errorMsg, setErrorMsg] = useState("");
-  const [data, setData] = useState();
+    const {id} = useParams();
+    const [fetchRes, setFetchRes] = useState("loading");
+    const [errorMsg, setErrorMsg] = useState("");
+    const [data, setData] = useState();
 
-  useEffect(() => {
-    (async () => {
-      let json;
-      console.log(id);
-      try {
-        json = await fetch(serverURL("articles/" + id), {
-          method: "GET",
-        }).then((response) => {
-          return response.json();
-        });
+    useEffect(() => {
+        (async () => {
+            let json;
+            console.log(id);
+            try {
+                json = await fetch(serverURL("articles/" + id), {
+                    method: "GET",
+                }).then((response) => {
+                    return response.json();
+                });
 
-        setFetchRes(json.success);
-        if (json.success) {
-          console.log(json.data);
-          setData(json.data);
+                setFetchRes(json.success);
+                if (json.success) {
+                    console.log(json.data);
+                    setData(json.data);
+                } else {
+                    setErrorMsg(json.message);
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        })();
+        // eslint-disable-next-line
+    }, []);
+
+    const addToCart = (articleId, quantity) => {
+        const existingCart = JSON.parse(localStorage.getItem("packages")) || [];
+        const cartPackageIndex = existingCart.findIndex(
+            (cartPackage) => cartPackage.articles.find((article) => article.article_id === articleId)
+        );
+        if (cartPackageIndex !== -1) {
+            existingCart[cartPackageIndex].articles.find(
+                (article) => article.article_id === articleId
+            ).quantity += quantity;
         } else {
-          setErrorMsg(json.message);
+            const articles = [{article_id: articleId, quantity}];
+            existingCart.push({
+                articles
+            });
         }
-      } catch (e) {
-        console.log(e);
-      }
-    })();
-    // eslint-disable-next-line
-  }, []);
 
-  switch (fetchRes) {
-    case "loading":
-      return (
-        <ArticleContainer>
-          <AnyText width="60">Product name</AnyText>
-          <AnyText width="80">
-            Description is loading, please wait until server response
-          </AnyText>
-        </ArticleContainer>
-      );
-    case true:
-      return (
-        <>
-          <ArticleContainer>
-            <AnyText width="60">{data?.name ?? "default_name"}</AnyText>
-            <Linebreak />
-            <AnyImage width="30" bin={data?.image?.[0] ?? null}></AnyImage>
-            <AnyDiv width="60">
-              <AnyText width="100">
-                {data?.description ?? "default_desc"}
-              </AnyText>
-              <AnyText width="100" color={data?.quantity > 0 ? "" : "red"}>
-                {data?.quantity === undefined
-                  ? "Unknown remainign quantity"
-                  : // eslint-disable-next-line
-                  data.quantity === 0
-                  ? "rupture de stock"
-                  : data.quantity +
-                    ` article${data.quantity > 1 ? "s" : ""} restant${
-                      data.quantity > 1 ? "s" : ""
-                    }`}
-              </AnyText>
-              <AnyText width="50">{data?.price ?? "??? "}€</AnyText>
-            </AnyDiv>
-            <AnyText width="80">{data?.specs ?? "default_specs"}</AnyText>
-          </ArticleContainer>
-        </>
-      );
-    case false:
-      return <AnyText width="80">{errorMsg}</AnyText>;
-    default:
-      <ArticleContainer>
-        <AnyText width="80">
-          There is an unrecognized error, good luck !
-        </AnyText>
-      </ArticleContainer>;
-  }
+        localStorage.setItem("packages", JSON.stringify(existingCart));
+    };
+    console.log(id)
+    switch (fetchRes) {
+        case "loading":
+            return (
+                <ArticleContainer>
+                    <AnyText width="60">Product name</AnyText>
+                    <AnyText width="80">
+                        Description is loading, please wait until server response
+                    </AnyText>
+                </ArticleContainer>
+            );
+        case true:
+            return (
+                <>
+                    <ArticleContainer>
+                        <AnyText width="60">{data?.name ?? "default_name"}</AnyText>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            type="submit"
+                            width={"100"}
+                            mb={"5"}
+                            onClick={() => addToCart(id, 1)}
+                        >
+                            Add to cart
+                        </Button>
+                        <Linebreak/>
+                        <AnyImage width="30" bin={data?.image?.[0] ?? null}></AnyImage>
+                        <AnyDiv width="60">
+                        </AnyDiv>
+                    </ArticleContainer>
+                </>
+            );
+        case false:
+            return <AnyText width="80">{errorMsg}</AnyText>;
+        default:
+            return (
+                <ArticleContainer>
+                    <AnyText width="80">There is an unrecognized error, good luck !</AnyText>
+                </ArticleContainer>
+            );
+    }
 }
