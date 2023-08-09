de80264b03ca6eec1ee250c2518eb7598f61d046
maximaron <maksym.prokopovych@epitech.eu>
2023-08-09 15:39:05 +0200

    Added aticle context so now we show in cart all what was added by user. Also we add all to the local storage.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import Router from "./router";
-import { useState } from "react";
-import { UserContext } from "./context/UserContext";
+import {useState} from "react";
+import {UserContext} from "./context/UserContext";
+import {ArticleContext} from "./context/articleContext";
 
 function App() {
-  const [user, setUser] = useState({});
+    const [user, setUser] = useState({});
+    const [article, setArticle] = useState({});
 
-  return (
-    <div className="App">
-      <UserContext.Provider value={{ user, setUser }}>
-        <Router />
-      </UserContext.Provider>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ArticleContext.Provider value={{article, setArticle}}>
+                <UserContext.Provider value={{user, setUser}}>
+                    <Router/>
+                </UserContext.Provider>
+            </ArticleContext.Provider>
+        </div>
+    );
 }
 
 export default App;
